a8de686909b395c969b3cf2587dfc3ff90142cd3
joseolinda <joseolindad@yahoo.com.br>
2022-08-29 16:14:51 -0300

    Base do card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PokeCards from './components/PokeCards'
 import api from './services/api'
 
 function App() {
   const [pokemons, setPokemons] = useState([])
 
+
+  const getPokemons = async () => {
+    const listPokemons = await api
+                                .get("121")
+                                .then(response => response.data)
+                                .catch(err => console.log(err))
+                               
+    console.log(listPokemons) 
+    setPokemons([...pokemons, listPokemons])                           
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+
   return (
     <div className="App">
-      <PokeCards />
-      <PokeCards />
-      <PokeCards />
-      <PokeCards />
+      <PokeCards pokemon={pokemons[0]} />
     </div>
   )
 }
