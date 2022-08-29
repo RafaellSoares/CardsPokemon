ea93ba3a047cbd974ec78b40d8da6d1b2bf1747d
joseolinda <joseolindad@yahoo.com.br>
2022-08-29 16:40:35 -0300

    Loop pronto

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,36 @@ function App() {
   const [pokemons, setPokemons] = useState([])
 
 
-  const getPokemons = async () => {
-    const listPokemons = await api
-                                .get("121")
-                                .then(response => response.data)
-                                .catch(err => console.log(err))
-                               
-    console.log(listPokemons) 
-    setPokemons([...pokemons, listPokemons])                           
+  const getPokemons = async (total) => {
+    const listPokemons = []
+
+    for(let i = 1; i <= total; i++) {
+      const newPokemon = await api
+        .get(`${i.toString()}`)
+        .then(response => response.data)
+        .catch(err => console.log(err))
+
+
+      listPokemons.push(newPokemon)  
+    }
+
+    setPokemons([...pokemons, ...listPokemons])
+
+    console.log(pokemons)  
   }
 
   useEffect(() => {
-    getPokemons()
+    getPokemons(50)
   }, [])
 
   return (
-    <div className="App">
-      <PokeCards pokemon={pokemons[0]} />
+    <div className="App container">
+      <h1>Pokemons Primeira Geração</h1>
+      <div className="cards">
+        {pokemons?.map(poke => {
+          return <PokeCards pokemon={poke} />
+        })}
+      </div>
     </div>
   )
 }
--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,11 +1,13 @@
 const PokeCards = function(props) {
     return (
-        <div className={"card " + props.pokemon?.tipo_base}>
-            <span className={"id " + props.pokemon?.tipo_base}>{props.pokemon?.id}</span>
+        <div className={"card " + props.pokemon?.types[0].type.name}>
+            <span className={"id " + props.pokemon?.types[0].type.name}>{props.pokemon?.id}</span>
             <img src={props.pokemon?.sprites.other.dream_world.front_default} alt="" className="photo" />
-            <span className={"name " + props.pokemon?.tipo_base}>{props.pokemon?.name}</span>
+            <span className={"name " + props.pokemon?.types[0].type.name}>{props.pokemon?.name}</span>
             <ul className="list_types">
-                {props.pokemon?.tipos}
+                {props.pokemon?.types.map(tipo => {
+                    return <li className={tipo.type.name}>{tipo.type.name}</li>
+                })}
             </ul>
         </div>
     );
